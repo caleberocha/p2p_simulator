1d85a6353dbc420ae8da4c8d6009713582da1feb
Calebe Rocha <angelo.calebe@edu.pucrs.br>
2020-10-25 16:33:18 -0300

    URL do server pode ser informada por parâmetro

--- peer/__main__.py
+++ peer/__main__.py
@@ -1,12 +1,18 @@
+import os
 import sys
 from .peer import Peer
 from . import constants
 
 if __name__ == "__main__":
     try:
-        port = int(sys.argv[1])
+        listen_port = int(sys.argv[1])
     except (ValueError, IndexError):
-        port = constants.LISTEN_PORT
+        listen_port = constants.LISTEN_PORT
 
-    peer = Peer("http://127.0.0.1:5000", port)
+    try:
+        server = sys.argv[2]
+    except IndexError:
+        server = constants.SERVER_URL
+
+    peer = Peer(server, listen_port)
     peer.start()
--- peer/constants.py
+++ peer/constants.py
@@ -1,10 +1,11 @@
-DIRECTORY_TO_SHARE = "$HOME/ws"
+DIRECTORY_TO_SHARE = "/usr/src/app/peer"
 
 LISTEN_PORT = 5001
 ALIVE_TIME = 5
 
 FILE_NOT_FOUND = bytes("_FILE_NOT_FOUND_", "utf-8")
 
+SERVER_URL = "http://p2pserver:5000"
 REQ_REGISTER = "/register"
 REQ_OFFERFILES = "/offerfiles"
 REQ_IAMALIVE = "/iamalive"
